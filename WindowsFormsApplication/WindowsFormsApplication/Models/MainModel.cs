0d99a3b017784db8211daff323ae8abd0a067f8b
danillykk <danillykk@gmail.com>
2016-09-13 18:19:42 +0300

    WindowsFormsApplication

---
+++ WindowsFormsApplication/WindowsFormsApplication/Models/MainModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApplication.Interface;
+
+namespace WindowsFormsApplication.Models
+{
+    public class MainModel : IModel
+    {
+        private IPresenter _presenter;
+
+        public MainModel(IPresenter presenter)
+        {
+            _presenter = presenter;
+        }
+
+        public string GetText(string filename)
+        {
+            string text = "";
+            try
+            {
+                File.ReadAllText(filename);
+            }
+            catch (Exception)
+            {
+                text = "Error, dude";
+            }
+            return text;
+        }
+    }
+}
