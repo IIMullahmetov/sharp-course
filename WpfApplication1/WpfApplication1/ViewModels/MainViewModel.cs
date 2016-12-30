2d34b0bdcc3327771e86223025795cd2c6c159ce
danillykk <danillykk@gmail.com>
2016-12-30 19:56:43 +0300

    add project

---
+++ WpfApplication1/WpfApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿/*
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using WpfApplication1.Models;
+//using GongSolutions.Wpf.DragDrop;
+using System.Windows;
+using System;
+
+namespace WpfApplication1.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<NoteViewModel> TasksList { get; set; }
+        public ObservableCollection<NoteViewModel> ActiveTasksList { get; set; }
+        public ObservableCollection<NoteViewModel> DoneTasksList { get; set; }
+
+        public MainViewModel()
+        {
+            //NotesList = new ObservableCollection<NoteViewModel>(notes.Select(n => new NoteViewModel(n)));
+        }
+
+        public MainViewModel(IEnumerable<Note> notes, DataBaseContext dbContext)
+        {
+            var enumarable = notes as IList<Note> ?? notes.ToList();
+
+            var todoData = enumarable.Where(t => t.NoteCategory.Equals("Goals"));
+            TasksList = new ObservableCollection<NoteViewModel>(todoData.Select(x => new NoteViewModel(x, dbContext)));
+
+            var activeData = enumarable.Where(t => t.NoteCategory.Equals("Active"));
+            ActiveTasksList = new ObservableCollection<NoteViewModel>(activeData.Select(x => new NoteViewModel(x, dbContext)));
+
+            var doneData = enumarable.Where(t => t.NoteCategory.Equals("Done"));
+            DoneTasksList = new ObservableCollection<NoteViewModel>(doneData.Select(x => new NoteViewModel(x, dbContext)));
+
+            //TasksList = new ObservableCollection<TaskViewModel>(tasks.Select(b => new TaskViewModel(b, dbContext));
+            //ActiveTasksList = new ObservableCollection<TaskViewModel>(tasks.Select(b => new TaskViewModel(b, dbContext)));
+
+        }
+    }
+}
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using WpfApplication1.Models;
+using System.Windows;
+using System;
+using WpfApplication1.ViewModels;
+
+namespace WpfApplication1.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<NoteViewModel> TasksList { get; set; }
+        public ObservableCollection<NoteViewModel> ActiveTasksList { get; set; }
+        public ObservableCollection<NoteViewModel> DoneTasksList { get; set; }
+
+        public MainViewModel()
+        {
+
+
+        }
+        #region Constructor
+
+        public MainViewModel(IEnumerable<Note> notes, DataBaseContext dbContext)
+        {
+            var enumarable = notes as IList<Note> ?? notes.ToList();
+
+            var todoData = enumarable.Where(t => t.NoteCategory.Equals("Goals"));
+            TasksList = new ObservableCollection<NoteViewModel>(todoData.Select(x => new NoteViewModel(x, dbContext)));
+
+            var activeData = enumarable.Where(t => t.NoteCategory.Equals("Active"));
+            ActiveTasksList = new ObservableCollection<NoteViewModel>(activeData.Select(x => new NoteViewModel(x, dbContext)));
+
+            var doneData = enumarable.Where(t => t.NoteCategory.Equals("Done"));
+            DoneTasksList = new ObservableCollection<NoteViewModel>(doneData.Select(x => new NoteViewModel(x, dbContext)));
+
+            //TasksList = new ObservableCollection<TaskViewModel>(tasks.Select(b => new TaskViewModel(b, dbContext));
+            //ActiveTasksList = new ObservableCollection<TaskViewModel>(tasks.Select(b => new TaskViewModel(b, dbContext)));
+
+        }
+
+
+        #endregion
+
+    }
+}
+
---
+++ WpfApplication1/WpfApplication1/Views/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TodoTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Binding binding = new Binding();
+
+            binding.ElementName = "TodoTasks"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            TextName.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+        }
+
+        private void ActiveTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Binding binding = new Binding();
+
+            binding.ElementName = "doneName"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            TextName.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+        }
+
+        private void DoneTasks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Binding binding = new Binding();
+
+            binding.ElementName = "doneName"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            TextName.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            StackPanel sp = (sender as StackPanel);
+            string nr = (sp.Children[1] as TextBlock).Text;
+            Console.WriteLine(nr);
+        }
+    }
+}
